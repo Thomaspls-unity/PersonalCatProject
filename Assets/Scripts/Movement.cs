5d5e74337f2912b519fee2a8e826e90209dc8357
Thomaspls-unity <Thomas.m.higgins1998@gmail.com>
2023-12-14 16:32:26 +0000

    Added a jump
    -Added more character movement with a jump, still not fully where I want it.
    -Added in the scripts I believe I'll need for the future
    -Collision scripts started but deletes the wrong game object

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -6,8 +6,10 @@ public class Movement : MonoBehaviour
 {
     public Rigidbody2D catRb;
     public float speed = 1f;
+    public float jumpHeight;
     public float horizontalInput;
     public Vector2 moveDirection;
+    public Transform translate;
 
     // Start is called before the first frame update
     void Start()
@@ -18,14 +20,22 @@ public class Movement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Input.GetKeyDown(KeyCode.W);
-        
         horizontalInput = Input.GetAxis("Horizontal");
         moveDirection = new Vector2 (horizontalInput, 0);
+        UpAndDown();
     }
 
     private void FixedUpdate()
     {
         catRb.velocity += moveDirection.normalized * speed;
+        //catRb.velocity += jumpDirection.normalized * speed;
+    }
+
+    private void UpAndDown()
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            transform.Translate(Vector2.up * Time.deltaTime * jumpHeight);
+        }
     }
 }
